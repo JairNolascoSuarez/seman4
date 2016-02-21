a8b3f0f4d7b4a0bf75c43370fb05bf76cba8a70a
Jair Nolasco <jairman@macbook-air-de-jair.local>
2016-02-21 17:03:38 -0600

    Initial Commit

---
+++ week_4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  week_4
+//
+//  Created by jair nolasco on 21/02/16.
+//  Copyright © 2016 jair nolasco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
